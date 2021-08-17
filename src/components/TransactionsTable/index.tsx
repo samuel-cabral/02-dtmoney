ab9d14cdef21e7421ca7886f9e0e9eac0a9819da
Samuel Cabral <samuelcabral.mail@gmail.com>
2021-08-17 01:30:32 -0300

    Set negative signal to withdraw transactions

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -21,10 +21,20 @@ export function TransactionsTable() {
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
               <td className={transaction.type}>
-                {new Intl.NumberFormat('pt-BR', {
-                  style: 'currency',
-                  currency: 'BRL',
-                }).format(transaction.amount)}
+                {transaction.type === 'deposit' ? (
+                  new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.amount)
+                ) : (
+                  <>
+                    -
+                    {new Intl.NumberFormat('pt-BR', {
+                      style: 'currency',
+                      currency: 'BRL',
+                    }).format(transaction.amount)}
+                  </>
+                )}
               </td>
               <td>{transaction.category}</td>
               <td>
